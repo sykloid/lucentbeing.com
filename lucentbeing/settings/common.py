8bfcd49fc3439f9c2efc00a37f41692cff770887
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-27 19:42:27 +0530

    Add basic role and locale settings.

--- lucentbeing/settings/common.py
+++ lucentbeing/settings/common.py
@@ -4,3 +4,18 @@ import os
 
 # Paths and URLs.
 SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+ADMINS = (
+    ('P.C. Shyamshankar', 'sykora@lucentbeing.com'),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = 'Asia/Calcutta'
+
+LANGUAGE_CODE = 'en-us'
+
+USE_I18N = False
+USE_L10N = False
+
+SITE_ID = 1
