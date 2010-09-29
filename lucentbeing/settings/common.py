06e3090f5dd11d1a6337c627fe3c01f1df86083a
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-09-29 14:24:34 +0530

    Add TEMPLATE_CONTEXT_PROCESSORS setting, including core's static_url.

--- lucentbeing/settings/common.py
+++ lucentbeing/settings/common.py
@@ -39,6 +39,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.contrib.messages.context_processors.messages',
+
+    'lucentbeing.core.context_processors.static_url',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
